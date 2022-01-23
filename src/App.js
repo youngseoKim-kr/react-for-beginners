3b304fdebb601d75e3681014e8a9443912636615
youngseoKim-kr <ysk5754@naver.com>
2022-01-23 19:54:56 +0900

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+function Hello() {
+  function byFn() {
+    console.log("bye:(");
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byFn;
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"} </button>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import propTypes from "prop-types";
+import styled from "./Button.module.css";
+
+function Button({ text }) {
+  return <button className={styled.btn}>{text}</button>;
+}
+
+Button.prototype = {
+  text: propTypes.string.isRequired,
+};
+export default Button;
